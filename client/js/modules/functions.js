a695e86c994d0d974134e9aa2492d717e40ef153
marco sanchez <masd_123@hotmail.com>
2025-09-19 23:40:31 -0300

    mobile menu fixed

--- client/js/modules/functions.js
+++ client/js/modules/functions.js
@@ -1,10 +1,19 @@
 import { mobileHidde, lineOne, lineTwo, lineThree } from "./domSelectors.js";
 
+const mobileMenu = document.querySelector(".mobileMenuDisplayedItems");
+const menuLinks = document.querySelectorAll(".menu-link");
+
 const displayMenu = () => {
   lineOne.classList.toggle("line-one");
   lineTwo.classList.toggle("line-two");
   lineThree.classList.toggle("line-three");
   mobileHidde.classList.toggle("displayMenu");
 };
+
+menuLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    mobileMenu.classList.remove("displayMenu");
+  });
+});
 
 export { displayMenu };
